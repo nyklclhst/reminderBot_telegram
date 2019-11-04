2757a3adaaa4d65557c4fd3f959b2d749ccb6333
nyklclhst <irfan.aliyafi@outlook.co.id>
2019-11-05 02:50:22 +0700

    update script

--- main.py
+++ main.py
@@ -10,7 +10,31 @@ tdy = datetime.today().strftime('%A')
 
 # Connection to mysql
 def openCon():
-    return pymysql.connect('localhost','<your username>','<your password>','tele_bot_data')
+    return pymysql.connect('localhost','<your mysql username>','<your mysql password>','tele_bot_data')
+
+# Check date time
+def get_date(chat_id):
+    if chat_id == '<your admin chat id>':
+        telegram_bot.sendMessage(chat_id,str(datetime.now()))
+    else :
+        telegram_bot.sendMessage(chat_id,str('Sorry, you cannot do this command!'))
+
+# check user registered or no
+def check_user(chat_id):
+    db = openCon()
+    cursor = db.cursor()
+    sql = 'select id from users where tele_id=%s' % chat_id
+    try:
+        cursor.execute(sql)
+        res = cursor.fetchone()
+        if res != None:
+            return True
+        else:
+            return False
+    except pymysql.Error as e:
+        print(e)
+        return False
+    db.close()
 
 # Function /start command
 def start(chat_id):
@@ -31,23 +55,27 @@ def start(chat_id):
 
 # Function /hi command
 def hi(chat_id):
-    if chat_id == "<your chat_id>":
+    if chat_id == '<your admin chat id>':
         telegram_bot.sendMessage (chat_id, str("Hi!"))
     else :
         telegram_bot.sendMessage (chat_id, str("You are not allowed to do this!"))
 
 # Function /help command
 def help(chat_id):
-    h_cmd = ['/hi','/help','/regis','/myjadwal','/addjadwal']
+    h_cmd = ['/hi','/help','/regis','/myjadwal','/addjadwal','/minggu','/date']
     h_desc = ['Are you alive?','See this message','Register your id to our database',
-        'To see all your jadwal','To adding new Jadwal']
+        'To see all your jadwal','To adding new Jadwal','To see minggu','To get System Datetime']
     resp = ''
-    if chat_id == "<your chat_id>":
+    if chat_id == '<your admin chat id>':
         for i in range(len(h_cmd)):
             resp += '%s - %s \n' % (h_cmd[i], h_desc[i])
     else :
         h_cmd.pop(0)
         h_desc.pop(0)
+        h_cmd.pop(5)
+        h_desc.pop(5)
+        h_cmd.pop(6)
+        h_desc.pop(6)
         for i in range(len(h_cmd)):
             resp += '%s - %s \n' % (h_cmd[i], h_desc[i])
     telegram_bot.sendMessage(chat_id,str(resp))
@@ -58,26 +86,29 @@ def myjadwal(chat_id):
     cursor = db.cursor()
     lst = []
     resp = ''
-    sql = ('select c.day, c.shift, c.status from users a left join '
-        'jadwal b on (b.user_id = a.id) left join day c on (c.id = b.day_id) '
-        'where a.tele_id = %s' % chat_id)
-    try:
-        cursor.execute(sql)
-        res = cursor.fetchall()
-        if res[0][0] != None:
-            for row in res:
-                day = row[0]
-                shift = row[1]
-                status = row[2]
-                lst.append('%s(%d) - shift: %d' % (day,status,shift))
-            for i in range(len(lst)):
-                resp += lst[i]+'\n'
-        else:
-            resp = ('Ooops... Looks like you haven\'t have any Jadwal. Please input '
-                'your jadwal with /addjadwal')
-    except pymysql.Error as e:
-        print(e)
-        resp = 'Ooops... Something went wrong!'
+    if check_user(chat_id):
+        sql = ('select c.day, c.shift, c.status from users a left join '
+            'jadwal b on (b.user_id = a.id) left join day c on (c.id = b.day_id) '
+            'where a.tele_id = %s' % chat_id)
+        try:
+            cursor.execute(sql)
+            res = cursor.fetchall()
+            if res[0][0] != None:
+                for row in res:
+                    day = row[0]
+                    shift = row[1]
+                    status = row[2]
+                    lst.append('%s(%d) - shift: %d' % (day,status,shift))
+                for i in range(len(lst)):
+                    resp += lst[i]+'\n'
+            else:
+                resp = ('Ooops... Looks like you haven\'t have any Jadwal. Please input '
+                    'your jadwal with /addjadwal')
+        except pymysql.Error as e:
+            print(e)
+            resp = 'Ooops... Something went wrong!'
+    else:
+        resp = 'Hello, Welcome to Seculab Reminder Bot. Please do /regis to start.'
     telegram_bot.sendMessage(chat_id,str(resp))
     db.close()
 
@@ -124,17 +155,21 @@ def addJadwal(chat_id,day_id):
     cursor = db.cursor()
     sql = ('insert into jadwal (user_id, day_id) select a.id,b.id '
         'from users a left join day b on (b.id=%s) where a.tele_id = %s' % (day_id,chat_id))
-    try:
-        cursor.execute(sql)
-        db.commit()
-        telegram_bot.sendMessage(chat_id,str('Congratulation! You have added new jadwal!'))
-    except pymysql.Error as e:
-        print(e)
-        telegram_bot.sendMessage(chat_id,str('Ooops... Something went wrong!'))
+    if check_user(chat_id):
+        try:
+            cursor.execute(sql)
+            db.commit()
+            telegram_bot.sendMessage(chat_id,str('Congratulation! You have added new jadwal!'))
+        except pymysql.Error as e:
+            print(e)
+            telegram_bot.sendMessage(chat_id,str('Ooops... Something went wrong!'))
+    else:
+        resp = 'Hello, Welcome to Seculab Reminder Bot. Please do /regis to start.'
+        telegram_bot.sendMessage(chat_id,str(resp))
     db.close()
 
 def check_status(chat_id):
-    if chat_id == "<your chat_id>":
+    if chat_id == '<your admin chat id>':
         telegram_bot.sendMessage(chat_id,str(sts))
 
 def action(msg):
@@ -168,6 +203,8 @@ def action(msg):
             addJadwal(chat_id,cmd[1])
     if cmd[0] == '/minggu':
         check_status(chat_id)
+    if cmd[0] == '/date':
+        get_date(chat_id)
 
 def reminder(today=tdy,status=sts):
     usr_id = []
@@ -203,7 +240,7 @@ def check_sts():
     else:
         sts+=1
 
-telegram_bot = telepot.Bot('< TOKEN >')
+telegram_bot = telepot.Bot('<your telegram bot token>')
 print(telegram_bot.getMe())
 MessageLoop(telegram_bot, action).run_as_thread()
 print('Up and Running....')
